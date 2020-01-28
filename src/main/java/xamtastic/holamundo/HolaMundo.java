a53448fadc2e15252bcd86c9ec26a4a38926f110
javierxam <javierxam@gmail.com>
2020-01-28 06:42:08 +0100

    Primer commit del HolaMundo

---
+++ src/main/java/xamtastic/holamundo/HolaMundo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xamtastic.holamundo;
+
+/**
+ *
+ * @author xavi
+ */
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("HolaMundo by Xamtastic¡");
+        System.out.println("Version 1.0");
+    }
+}
